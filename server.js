97cc8fa701327d8c84dfb5833930dd6161822df0
Arvind Kumar Singh <arvind.singh_cs16@gla.ac.in>
2020-07-25 17:53:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const { bot } = require("./bot");
+bot.startPolling();
+
+app.get("/", (req, res) => {
+  res.send(
+    "Please visit this <a href='https://t.me/side_projects_bot'>link</a> to use the bot"
+  );
+});
+
+app.listen(PORT, console.log(`listening on ${PORT}`));
